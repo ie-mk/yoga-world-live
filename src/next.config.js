108276df52e126ddbd29ec9274eb32941b091ba7
Justinas Simutis <justinas.simutis@gmail.com>
2020-05-23 13:26:38 +0100

    cleanup

--- src/next.config.js
+++ src/next.config.js
@@ -14,11 +14,6 @@ module.exports = withCSS({
     alias['foundation'] = path.join(projectRoot, 'components/foundation');
     alias['modules'] = path.join(projectRoot, 'modules');
     alias['store'] = path.join(projectRoot, 'store');
-    //alias['apiPath'] = path.join(projectRoot, 'api/api');
-    // alias['apiPath'] = path.join(
-    //   projectRoot,
-    //   './api/api.min',
-    // );
     return config;
   },
 });
