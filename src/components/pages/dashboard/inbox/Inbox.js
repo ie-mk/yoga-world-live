82a249c33f08084d737f51c67f013dcb0771b74a
Justinas Simutis <justinas.simutis@gmail.com>
2020-05-25 19:20:39 +0100

    table refactor

--- src/components/pages/dashboard/inbox/Inbox.js
+++ src/components/pages/dashboard/inbox/Inbox.js
@@ -6,38 +6,33 @@ import { object } from 'firebase-functions/lib/providers/storage';
 const Inbox = () => {
   const messages = {
     '124jq23j234': {
-      senderId: '1',
+      senderId: '845235o2u35',
       senderName: 'John',
       email: 'some@email.com',
       senderPhone: '+44123874533',
       message: 'Some text message',
-      button: 'Reply',
     },
     '124jq23ddj234': {
-      senderId: '2',
+      senderId: '845235o2u35',
       senderName: 'Jack',
       email: 'some@email.com',
       senderPhone: '+44123874533',
       message: 'Some text message2',
-      button: 'Reply',
     },
   };
 
-  const inboxColumn = {
-    SNo: 'S.No',
-    studentName: 'Student Name',
-    email: 'Email',
-    phone: 'Phone',
-    message: 'Message',
-    actions: 'Actions',
-  };
-
-  const columnHeaders = Object.values(inboxColumn);
-  const columnValues = Object.values(messages);
+  const columnHeaders = [
+    'S.No',
+    'Student Name',
+    'Email',
+    'Phone',
+    'Message',
+    'Actions',
+  ];
 
   return (
     <ContainerBase margin="25px" marginRight="25px" marginTop="30px">
-      <Table columnHeaders={columnHeaders} columnValues={columnValues} />
+      <Table columnHeaders={columnHeaders} data={messages} />
     </ContainerBase>
   );
 };
--- src/components/pages/dashboard/table/Table.js
+++ src/components/pages/dashboard/table/Table.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import Styled from './Table.styles';
 
-const Table = ({ columnHeaders, columnValues }) => {
+const Table = ({ columnHeaders, data }) => {
+  const handleReply = messageId => {
+    // TODO
+  };
+
   const headers = columnHeaders.map((key, index) => (
     <Styled.Th>{columnHeaders[index]}</Styled.Th>
   ));
 
-  const columnVals = columnValues.map((columnValue, index) => (
-    <Styled.Tr>
-      {Object.values(columnValue).map((key, index) =>
-        key != 'Reply' ? (
-          <Styled.Td>{key}</Styled.Td>
-        ) : (
-          <Styled.Td>
-            <Styled.Button>{key}</Styled.Button>
-          </Styled.Td>
-        ),
-      )}
-    </Styled.Tr>
-  ));
+  const tableRows = Object.keys(data).map((id, idx) => {
+    const rowData = data[id];
+    if (!rowData) return null;
+
+    return (
+      <Styled.Tr key={id}>
+        <Styled.Td>{idx + 1}</Styled.Td>
+        <Styled.Td>{rowData.senderName}</Styled.Td>
+        <Styled.Td>{rowData.email}</Styled.Td>
+        <Styled.Td>{rowData.senderPhone}</Styled.Td>
+        <Styled.Td>{rowData.message}</Styled.Td>
+        <Styled.Td>
+          <Styled.Button onClick={() => handleReply(id)}>Reply</Styled.Button>
+        </Styled.Td>
+      </Styled.Tr>
+    );
+  });
   return (
     <Styled.Table>
       <Styled.Tr>{headers}</Styled.Tr>
-      {columnVals}
+      {tableRows}
     </Styled.Table>
   );
 };
