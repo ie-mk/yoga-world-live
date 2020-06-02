c08897d67ba57a495cbb079db043f0758e7a9dcc
Lenovo <kesavamannem@gmail.com>
2020-06-02 21:38:55 +0530

    done some changes

--- src/components/pages/dashboard/courses/courseForm/CourseForm.jsx
+++ src/components/pages/dashboard/courses/courseForm/CourseForm.jsx
@@ -4,6 +4,21 @@ import CourseChapter01 from '../courseChapter01/CourseChapter01';
 import { ContainerBase } from '../../../../foundation';
 import CourseChapter from '../courseChapter/CourseChapter';
 
+const mockChapterData = {
+  someId: {
+    chaptertitle: 'someTitle1',
+    description: 'Some description1',
+    videolink: 'someLink1',
+    assignment: 'link to the task1',
+  },
+  someId2: {
+    chaptertitle: 'someTitle',
+    description: 'Some description',
+    videolink: 'someLink',
+    assignment: 'link to the task',
+  },
+};
+
 const CourseForm = () => {
   return (
     <ContainerBase>
