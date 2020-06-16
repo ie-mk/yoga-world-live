a432f9afc3a98c8d50b70a7734858cd919b923d9
Rajesh <rajesh.ganne222@gmail.com>
2020-06-16 20:42:31 +0530

    cs-28

--- src/components/foundation/learningPathCard/LearningPathCard.jsx
+++ src/components/foundation/learningPathCard/LearningPathCard.jsx
@@ -27,7 +27,7 @@ const Wrapper = styled.div`
 
 const LearningPathCard = ({ imageSrc, title }) => {
   return (
-    <Wrapper background={background}>
+    <Wrapper>
       <ResponsiveImage height="120px" width="120px" src={imageSrc} />
       <PathTitle text={title} />
     </Wrapper>
