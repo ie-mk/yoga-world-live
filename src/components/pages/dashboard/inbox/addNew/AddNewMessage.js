1ddea615ee5c46f4dfad177b44d92996cc9b2ad2
Justinas Simutis <justinas.simutis@gmail.com>
2020-06-25 17:53:02 +0100

    fix fetch public users data fix

--- src/components/pages/dashboard/inbox/addNew/AddNewMessage.js
+++ src/components/pages/dashboard/inbox/addNew/AddNewMessage.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from 'react';
+import React, { useState, memo, useEffect } from 'react';
 import Styled from './AddNewMessagestyles';
 import { connect } from 'react-redux';
 import { Formik, ErrorMessage, Field } from 'formik';
@@ -6,8 +6,13 @@ import AdminInput from '../../../../foundation/input/AdminInput';
 import AdminTextArea from '../../../../foundation/textarea/AdminTextArea';
 import AdminUploadImage from '../../../../foundation/pictureUploader/PictureUploader';
 import Button from '../../../../foundation/button/Button';
+import { userActions, resourceActions } from '../../../../../store/actions';
 
 let AddNewMessage = ({ dispatch, setNewAdd }) => {
+  useEffect(() => {
+    dispatch(userActions.fetchAllUsersPublicInfo.request());
+  }, []);
+
   return (
     <div>
       <Formik
@@ -20,7 +25,7 @@ let AddNewMessage = ({ dispatch, setNewAdd }) => {
           setTimeout(() => setSubmitting(false), 1000);
         }}
       >
-        {({ values, handleSubmit, setFieldValue }) => (
+        {({ values, handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <Styled.InputRow>
               <AdminInput
