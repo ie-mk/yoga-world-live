e1e1dffd813ac231408c527eb4d261da4ef824df
Justinas Simutis <justinas.simutis@gmail.com>
2020-08-31 19:35:32 +0100

    fix hero image change

--- src/components/heros/heroFront/HeroFront.jsx
+++ src/components/heros/heroFront/HeroFront.jsx
@@ -26,44 +26,26 @@ const HeroFront = () => {
     );
   };
 
-  var imgsrc = ' ';
-
-  if (window.innerWidth < 756) {
-    imgsrc = '/svg/Mobile_BG_V1.svg';
-  } else {
-    imgsrc = '/svg/Landing_Page_BG_Desktop.svg';
-  }
   return (
     <Styled.Wrapper>
-      {/* <ResponsiveImage
-        src="/svg/Landing_Page_BG_Desktop.svg"
-        height="350px"
+      <ResponsiveImage
+        src="/svg/Mobile_BG_V1.svg"
+        height="50vh"
         width="100%"
-        margin="50px 0 0 0"
         backgroundSize="contain"
+        margin="150px 0 0"
         mediaConfig={{
-          belowTablet: {
-            display: 'none',
+          aboveTablet: {
+            backgroundImage: 'url(/svg/Landing_Page_BG_Desktop.svg)',
+            margin: 0,
           },
         }}
-      /> */}
+      />
       <Styled.Content>
         <Styled.RowContainer>
           <Styled.TextWrapper>
             THE SMARTER WAY TO LEARN ANYTHING FROM HOME
           </Styled.TextWrapper>
-          <ResponsiveImage
-            src={imgsrc}
-            height="250px"
-            width="100%"
-            margin="0 0 50px 0"
-            backgroundSize="contain"
-            mediaConfig={{
-              aboveTablet: {
-                //  display: 'none',
-              },
-            }}
-          />
           <Styled.ButtonWrapper>
             <Button size="sm" type="primary" onClick={goToCourses}>
               VIEW COURSES
